a56fb6a8149da9a2ece70814a58a4a9ce2b849fb
zombiemcfly <zombiemcfly@gmail.com>
2017-02-13 11:05:51 -0600

    Debuggind error en servidor, comenté gran parte del código

--- CotizadorWeb/Cotizador/urls.py
+++ CotizadorWeb/Cotizador/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import url
-
 from .views import (
     index,
     nueva_cotizacion,
-    nuevo_producto,
-    consultar_cotizacion,
-    listar_cotizacion,
-    eliminar_cotizacion,
+    # nuevo_producto,
+    # consultar_cotizacion,
+    # listar_cotizacion,
+    # eliminar_cotizacion,
     )
 
 # from .printing import(
@@ -14,12 +13,12 @@ from .views import (
 #     )
 
 urlpatterns = [
-    url(r'^$', index),
+    url(r'^', index),
     url(r'^nueva/$', nueva_cotizacion),
-    url(r'^productos/$', nuevo_producto),
-    url(r'^consulta/(?P<id>\d+)/$', consultar_cotizacion),
-    url(r'^lista/$', listar_cotizacion),
-    url(r'^eliminar/$', eliminar_cotizacion),
-    #url(r'^pdf/$', generador_pdf),
+    # url(r'^productos/$', nuevo_producto),
+    # url(r'^consulta/(?P<id>\d+)/$', consultar_cotizacion),
+    # url(r'^lista/$', listar_cotizacion),
+    # url(r'^eliminar/$', eliminar_cotizacion),
+    # #url(r'^pdf/$', generador_pdf),
 
 ]
